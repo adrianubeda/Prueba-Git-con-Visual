b03d4c8cc8523b550d845cdf3eae76425e444ef1
adrianubeda <72019874+adrianubeda@users.noreply.github.com>
2020-11-09 13:12:39 +0100

    Agregar archivos de proyecto.

---
+++ Prueba Git con Visual/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prueba_Git_con_Visual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adios World!");
+        }
+    }
+}
